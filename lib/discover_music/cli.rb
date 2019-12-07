e02496f069c2377f679535cf3faf3c79d1ed6e23
Logan Smith <logsmith06@gmail.com>
2019-12-07 18:55:03 +0000

    added basic command line interface (subject to change)

--- lib/discover_music/cli.rb
+++ lib/discover_music/cli.rb
@@ -5,6 +5,7 @@ class DiscoverMusic::CLI
   def call
     list_artists
     menu
+    goodbye
   end
 
   def list_artists
@@ -16,7 +17,22 @@ class DiscoverMusic::CLI
   end
 
   def menu
-    puts "Type in the name of the artist that you would like to know more about: "
+    puts "Type in the name of the artist that you would like to know more about:"
+    chosen_artist = nil
+
+    while chosen_artist != "exit"
+      chosen_artist = gets.strip
+      case chosen_artist
+      when "1"
+        puts "..."
+      when "2"
+        puts "lll"
+      end
+    end
   end
+
+    def goodbye
+      puts "Thank you for using Discover Music!"
+    end
 
 end
