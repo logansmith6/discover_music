38ee341352a244d0746536f1615edd0f4d532988
Logan Smith <logsmith06@gmail.com>
2019-12-07 19:08:20 +0000

    added finishing touches to the user interface options

--- lib/discover_music/cli.rb
+++ lib/discover_music/cli.rb
@@ -2,12 +2,14 @@
 
 class DiscoverMusic::CLI
 
+  #call method calls the proper methods, in order
   def call
     list_artists
     menu
     goodbye
   end
 
+  #list_artists will list all available artists to choose from
   def list_artists
     puts "List of Artists:"
     puts <<-DOC.gsub /^\s*/, ''
@@ -16,23 +18,29 @@ class DiscoverMusic::CLI
     DOC
   end
 
+  #command line user interface menu
   def menu
-    puts "Type in the name of the artist that you would like to know more about:"
+    puts "Type in the name of the artist that you would like to know more about (type exit to quit or menu to get back to the artist list):"
     chosen_artist = nil
-
+    #loop to ask user for input until "exit" is typed
     while chosen_artist != "exit"
-      chosen_artist = gets.strip
+      chosen_artist = gets.strip.downcase
       case chosen_artist
       when "1"
         puts "..."
       when "2"
         puts "lll"
+      when "menu"
+        list_artists
+      else
+        puts "Invalid selection. Please try again!"
       end
     end
   end
 
-    def goodbye
-      puts "Thank you for using Discover Music!"
-    end
+  #goodbye says goodbye
+  def goodbye
+    puts "Thank you for using Discover Music!"
+  end
 
 end
