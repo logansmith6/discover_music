afdbf370754c59c5e721bec24fab3c834df87e33
Logan Smith <logsmith06@gmail.com>
2019-12-07 20:39:29 +0000

    beggining to work on a method that will return a list of all available artists to choose from

--- lib/discover_music/scrape.rb
+++ lib/discover_music/scrape.rb
@@ -3,6 +3,9 @@ class DiscoverMusic::Scrape
   attr_accessor :name, :albums, :url
   #the artist name chosen by the user will be passed in and then used to scrape a specific bio
 
+
+  #takes in a name from the artist class and converts it to be
+  #compatible with the website
   def self.url_conversion(name)
     @name = name
     chosen_artist = name.split(" ")
@@ -21,7 +24,8 @@ class DiscoverMusic::Scrape
       @url = chosen_artist
 
   end
-
+  #scraper class takes the converted url ending and uses it to scrape the
+  #specific web address of the user's chosen artist
   def self.scraper
     doc = Nokogiri::HTML(open("https://www.udiscovermusic.com/artists-a-z/#{@url.join}"))
     artist = doc.search("div.digging-deeper-text").text.strip
@@ -32,6 +36,8 @@ class DiscoverMusic::Scrape
     puts  "    " + artist
   end
 
-
+  def self.all_artists
+    doc = Nokogiri::HTML(open("https://www.udiscovermusic.com/artists-a-z/#{@url.join}"))
+  end
 
 end
